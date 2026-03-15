249eae4f5d53c8d8c466ca4db5aae83d7692c9e6
Nick Korintus <nicholas.korintus@gmail.com>
2026-03-15 17:53:13 -0400

    refactor: update mock db data to use global store

--- src/data/messagesStore.ts
+++ src/data/messagesStore.ts
@@ -4,13 +4,25 @@
 
 import { Message } from "@/types/message";
 
-let messages: Message[] = [
-  { id: 1, text: "Hello world" },
-  { id: 2, text: "Next.js example" }
-];
+// let messages: Message[] = [
+//   { id: 1, text: "Hello world" },
+//   { id: 2, text: "Next.js example" }
+// ];
+
+declare global {
+  var messagesStore: Message[] | undefined;
+}
+
+if (!global.messagesStore) {
+  global.messagesStore = [
+    { id: 1, text: "Hello world" },
+    { id: 2, text: "Next.js example" }
+  ];
+}
 
 export function getAllMessages(): Message[] {
-  return messages;
+  // return messages;
+  return global.messagesStore!;
 }
 
 export function insertMessage(text: string): Message {
@@ -19,6 +31,9 @@ export function insertMessage(text: string): Message {
     text
   };
 
-  messages.push(message);
+  // messages.push(message);
+  global.messagesStore!.push(message);
   return message;
 }
+
+console.log("messagesStore loaded");
